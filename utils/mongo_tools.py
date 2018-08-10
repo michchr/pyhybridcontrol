464abb34facc2a68645da28dbc7e81296e9a3cb3
Unknown <c.j.michalak@student.tudelft.nl>
2018-08-10 14:31:42 +0200

    Backup commit-MongoTools

--- utils/mongo_tools.py
+++ utils/mongo_tools.py
@@ -2,7 +2,7 @@ import sys
 import logging
 import pymongo as pym
 from pymongo import MongoClient, errors as PymErrors, CursorType
-from pymongo.collection import Collection
+from pymongo.collection import Collection, Cursor
 from pymongo.database import Database
 import pandas as pd
 import numpy as np
@@ -13,14 +13,26 @@ from functools import reduce
 pd.set_option('mode.chained_assignment', 'raise')
 # db.authenticate(MONGO_USER, MONGO_PASS)
 
-ch = logging.StreamHandler(sys.stderr)
-ch.setLevel(logging.INFO)
-ch.setFormatter(logging.Formatter(fmt='%(levelname)s:%(name)s:%(message)s'))
+console_hand = logging.StreamHandler(sys.stdout)
+console_hand.setLevel(logging.INFO)
+console_hand.setFormatter(logging.Formatter(fmt='%(levelname)s:%(name)s:%(message)s'))
 
-# logging.basicConfig(level=logging.WARNING)
-log = logging.getLogger(__name__)
-log.addHandler(ch)
-log.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.propagate = False
+if logger.hasHandlers(): logger.handlers = []
+logger.addHandler(console_hand)
+logger.setLevel(logging.INFO)
+
+_DEV_TAG_MAP = {
+    'dewh': 'GEY',
+    'pm': 'PM',
+    'inv': 'INV',
+}
+
+_DEV_DEFAULT_FIELDS = {
+    'dewh': ['Temp', 'Status'],
+    'pm': ['ForActiveEnergy_Tot', 'RevActiveEnergy_Tot', 'ActivePower_Tot', 'ApparentPower_Tot']
+}
 
 
 def deep_get(doc, key_list, default=None):
@@ -34,10 +46,8 @@ def deep_get(doc, key_list, default=None):
 
 
 class MongoInterface():
-    DEWH_DEFAULT_FIELDS = ['Temp', 'Status']
-    PM_DEFAULT_FIELDS = ['ForActiveEnergy_Tot', 'RevActiveEnergy_Tot', 'ActivePower_Tot', 'ApparentPower_Tot']
 
-    def __init__(self, collection=None, database=None, host='localhost', port=27017):
+    def __init__(self, database=None, collection=None, host='localhost', port=27017):
 
         if isinstance(collection, Collection):
             self._collection = collection
@@ -54,16 +64,16 @@ class MongoInterface():
                 raise ConnectionError('Could not connect to database')
 
             if database not in self._client.list_database_names():
-                log.info("Database: '%s', does not exist - creating new", database)
+                logger.info("Database: '%s', does not exist - creating new", database)
             self._database = self._client[database]
 
             if collection not in self._database.list_collection_names():
-                log.info("Collection: '%s', does not exist in Database: '%s' - creating new", collection,
-                      self._database.name)
+                logger.info("Collection: '%s', does not exist in Database: '%s' - creating new", collection,
+                            self._database.name)
             self._collection = self._database[collection]
 
-            log.info("Created MongoInterface using Database: '%s', and Collection: '%s'",
-                self._database.name, self._collection.name)
+            logger.info("Created MongoInterface using Database: '%s', and Collection: '%s'",
+                        self._database.name, self._collection.name)
 
     @property
     def client(self):
@@ -82,10 +92,11 @@ class MongoInterface():
         self._collection = collection
 
     def get_many_dev_cursor(self, device_type, dev_ids, self_col: Collection = None, start_time: DateTime = None,
-                            end_time: DateTime = None, limit=None, fields=None):
+                            end_time: DateTime = None, limit=0, fields=None):
 
-        if self_col is None:
-            self_col = self.collection
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
 
         if fields == 'all':
             select_all_fields = True
@@ -96,16 +107,15 @@ class MongoInterface():
         if (not isinstance(fields, list)) and (fields is not None):
             raise ValueError("fields must be list of strings or 'all'")
 
-        if device_type == 'dewh':
-            str_id_list = ["GEY" + str(dev_id) for dev_id in dev_ids]
+        mongo_dev_tag = _DEV_TAG_MAP.get(device_type)
+        mongo_dev_fields = _DEV_DEFAULT_FIELDS.get(device_type)
+
+        if mongo_dev_tag:
+            str_id_list = [mongo_dev_tag + str(dev_id) for dev_id in dev_ids]
             if fields is None:
-                fields = MongoInterface.DEWH_DEFAULT_FIELDS
-        elif device_type == 'pm':
-            str_id_list = ["PM" + str(dev_id) for dev_id in dev_ids]
-            if fields is None:
-                fields = MongoInterface.PM_DEFAULT_FIELDS
+                fields = mongo_dev_fields
         else:
-            raise ValueError('Device type does not exist or is not supported')
+            raise ValueError("Device type: '{}' does not exist or is not supported".format(device_type))
 
         dev_selection = [{'$and': [{str_id: {'$exists': 1}},
                                    {'$or': [
@@ -115,38 +125,36 @@ class MongoInterface():
                           } for str_id in str_id_list
                          ]
 
+        query = {'$and': [{"TimeStamp": {'$gte': start_time, '$lte': end_time}},
+                          {'$or': dev_selection}
+                          ]
+                 }
+
         projection = {"_id": 0, 'TimeStamp': 1}
         if select_all_fields:
             projection.update({str_id: 1 for str_id in str_id_list})
         else:
             projection.update({str_id + "." + field: 1 for field in fields for str_id in str_id_list})
 
-        query = [{'$and': [{"TimeStamp": {'$gte': start_time, '$lte': end_time}},
-                           {'$or': dev_selection}
-                           ]
-                  }, projection
-                 ]
+        cursor = self.collection.find(query, projection, sort=[("TimeStamp", 1)], limit=limit,
+                                      cursor_type=CursorType.NON_TAILABLE)
 
-        cur = self_col.find(*query, cursor_type=CursorType.NON_TAILABLE).sort("TimeStamp", 1)
-        if limit is not None:
-            cur = cur.limit(limit)
-
-        return cur
+        return cursor
 
     def get_one_dev_cursor(self, device_type, dev_id, self_col: Collection = None, start_time: DateTime = None,
-                           end_time: DateTime = None, limit=None, fields=None):
+                           end_time: DateTime = None, limit=0, fields=None):
 
         return self.get_many_dev_cursor(device_type, [dev_id], self_col=self_col, start_time=start_time,
                                         end_time=end_time, limit=limit, fields=fields)
 
     def get_many_dev_raw_dataframe(self, device_type, dev_ids, self_col: Collection = None, start_time: DateTime = None,
-                                   end_time: DateTime = None, limit=None, fields=None, cursor=None):
+                                   end_time: DateTime = None, limit=0, fields=None, cursor=None):
 
         if cursor is None:
             cursor = self.get_many_dev_cursor(device_type, dev_ids, self_col=self_col, start_time=start_time,
                                               end_time=end_time, limit=limit, fields=fields)
-
-        list_cur = list(cursor)
+        with cursor as cursor:
+            list_cur = list(cursor)
 
         if len(list_cur) == 0:
             raw_df = pd.DataFrame()
@@ -159,74 +167,93 @@ class MongoInterface():
         return raw_df
 
     def get_one_dev_raw_dataframe(self, device_type, dev_id, self_col: Collection = None, start_time: DateTime = None,
-                                  end_time: DateTime = None, limit=None, fields=None, cursor=None):
+                                  end_time: DateTime = None, limit=0, fields=None, cursor=None):
 
         return self.get_many_dev_raw_dataframe(device_type, [dev_id], self_col=self_col, start_time=start_time,
                                                end_time=end_time, limit=limit, fields=fields, cursor=cursor)
 
+    def get_dev_tags(self, device_type, dev_id, self_col: Collection = None):
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
+
+
+
+
+
     def set_string_field_to_datetime(self, self_col: Collection = None, field_tag='TimeStamp'):
-        if self_col is None:
-            self_col = self.collection
+
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
 
         # find docs with field tags that are not of datetime format
-        cur = self_col.find({field_tag: {'$not': {'$type': "date"}}}, {field_tag: 1})
+        query = {field_tag: {'$not': {'$type': "date"}}}
+        proj = {field_tag: 1}
 
-        bulk_requests = []
-        for doc in cur:
-            timestamp = parse_datetime(doc[field_tag])
-            bulk_requests.append(pym.UpdateOne({
-                '_id': doc['_id']
-            }, {
-                '$set': {
-                    field_tag: timestamp
-                }
-            }))
+        with self.collection.find(query, proj) as cursor:
+            bulk_requests = []
+            for doc in cursor:
+                timestamp = parse_datetime(doc[field_tag])
+                bulk_requests.append(pym.UpdateOne({
+                    '_id': doc['_id']
+                }, {
+                    '$set': {
+                        field_tag: timestamp
+                    }
+                }))
 
         if bulk_requests:
-            result = self_col.bulk_write(bulk_requests).modified_count
+            result = self.collection.bulk_write(bulk_requests).modified_count
         else:
             result = 0
 
-        print("Modified TimeStamp type for {} documents".format(result))
+        logger.info("Modified TimeStamp type for %s documents", result)
         return result
 
     # todo not completed
     def set_string_field_to_float(self, self_col: Collection = None, field_tag='GEY1.Temp'):
-        if self_col is None:
-            self_col = self.collection
+
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
 
         # find docs with field tags that are not of a certain type
-        cur = self_col.find({field_tag: {'$not': {'$type': "double"}}}, {field_tag: 1})
+        query = {field_tag: {'$not': {'$type': "double"}}}
+        proj = {field_tag: 1}
 
-        bulk_requests = []
-        for doc in cur:
-            try:
-                value = float(deep_get(doc, field_tag.split('.')))
-            except ValueError:
-                value = float('nan')
-            except TypeError:
-                value = float('nan')
+        with self.collection.find(query, proj) as cursor:
+            bulk_requests = []
+            for doc in cursor:
+                try:
+                    value = float(deep_get(doc, field_tag.split('.')))
+                except ValueError:
+                    value = float('nan')
+                except TypeError:
+                    value = float('nan')
 
-            bulk_requests.append(pym.UpdateOne({
-                '_id': doc['_id']
-            }, {
-                '$set': {
-                    field_tag: value
-                }
-            }))
+                bulk_requests.append(pym.UpdateOne({
+                    '_id': doc['_id']
+                }, {
+                    '$set': {
+                        field_tag: value
+                    }
+                }))
 
         if bulk_requests:
-            result = self_col.bulk_write(bulk_requests).modified_count
+            result = self.collection.bulk_write(bulk_requests).modified_count
         else:
             result = 0
 
-        print("Modified field type for {} documents".format(result))
+        logger.info("Modified field type for %s documents", result)
         return result
 
-    def append_from_collection(self, from_col: Collection, self_col: Collection = None, limit=None, newest=True,
+    def append_from_collection(self, from_col: Collection, self_col: Collection = None, limit=0, newest=True,
                                override=False):
-        if self_col is None:
-            self_col = self.collection
+
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
 
         if from_col.count_documents({}) == 0:
             raise ValueError('Source _collection is empty, nothing to copy')
@@ -234,28 +261,30 @@ class MongoInterface():
             raise ValueError('from_col == self_col, cannot append to self from self, unless override is set')
 
         order = pym.DESCENDING if newest else pym.ASCENDING
-        if limit is None:
-            cur = from_col.find({}, {'_id': 0}).sort('_id', order)
-        else:
-            cur = from_col.find({}, {'_id': 0}).sort('_id', order).limit(limit)
+        query = {}
+        proj = {'_id': 0}
+        sort = [('_id', order)]
 
-        requests = []
-        for doc in cur:
-            requests.append(pym.InsertOne(doc))
+        with from_col.find(query, proj, sort=sort, limit=limit) as cursor:
+            requests = []
+            for doc in cursor:
+                requests.append(pym.InsertOne(doc))
 
         try:
-            result = self_col.bulk_write(requests)
+            result = self.collection.bulk_write(requests)
         except PymErrors.BulkWriteError as bwe:
             raise IndexError("BulkWriteError with errmsg: {}".format(bwe.details['writeErrors'][0]['errmsg'])) from None
 
-        return result.inserted_count
+        num_appended = result.inserted_count
+        logger.info("Appended %s documents", num_appended)
+        return num_appended
 
-    def clone_from_collection(self, from_col: Collection, self_col: Collection = None, limit=None, newest=True,
+    def clone_from_collection(self, from_col: Collection, self_col: Collection = None, limit=0, newest=True,
                               override=False):
 
         if (self_col is not None) and (self_col != self._collection):
             self.collection = self_col
-            print("MongoInterface now using Collection: '{}'".format(self.collection.name))
+            logger.info("MongoInterface now using Collection: '{%s}'", self.collection.name)
 
         order = pym.DESCENDING if newest else pym.ASCENDING
         if limit is None:
@@ -278,7 +307,7 @@ class MongoInterface():
             from_col.database.command('aggregate', from_col.name, pipeline=pipeline, explain=False)
 
         num_cloned = self.collection.count_documents({})
-        print("Cloned {} documents".format(num_cloned))
+        logger.info("Cloned %s documents", num_cloned)
         return num_cloned
 
     def close(self):
@@ -289,7 +318,7 @@ if __name__ == '__main__':
     import timeit
     from timeit import default_timer as Timer
 
-    mi = MongoInterface(collection='Kikuyu', database='site_data')
+    mi = MongoInterface(database='site_data', collection='Kikuyu')
     # mi.clone_collection(kik)
     mi.set_string_field_to_datetime()
     mi.collection.create_index("TimeStamp")
