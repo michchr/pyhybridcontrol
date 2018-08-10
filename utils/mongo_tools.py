de9c8b32c9baf3a56c4f48ad0baa2029e3873a19
Unknown <c.j.michalak@student.tudelft.nl>
2018-08-10 10:21:30 +0200

    Back_up commit - mongo_tools in beta

---
+++ utils/mongo_tools.py
@@ -0,0 +1,372 @@
+import sys
+import logging
+import pymongo as pym
+from pymongo import MongoClient, errors as PymErrors, CursorType
+from pymongo.collection import Collection
+from pymongo.database import Database
+import pandas as pd
+import numpy as np
+from datetime import datetime as DateTime
+from dateutil.parser import parse as parse_datetime
+from functools import reduce
+
+pd.set_option('mode.chained_assignment', 'raise')
+# db.authenticate(MONGO_USER, MONGO_PASS)
+
+ch = logging.StreamHandler(sys.stderr)
+ch.setLevel(logging.INFO)
+ch.setFormatter(logging.Formatter(fmt='%(levelname)s:%(name)s:%(message)s'))
+
+# logging.basicConfig(level=logging.WARNING)
+log = logging.getLogger(__name__)
+log.addHandler(ch)
+log.setLevel(logging.INFO)
+
+
+def deep_get(doc, key_list, default=None):
+    def reducer(d, key):
+        if isinstance(d, dict):
+            return d.get(key)
+        else:
+            return default
+
+    return reduce(reducer, key_list, doc)
+
+
+class MongoInterface():
+    DEWH_DEFAULT_FIELDS = ['Temp', 'Status']
+    PM_DEFAULT_FIELDS = ['ForActiveEnergy_Tot', 'RevActiveEnergy_Tot', 'ActivePower_Tot', 'ApparentPower_Tot']
+
+    def __init__(self, collection=None, database=None, host='localhost', port=27017):
+
+        if isinstance(collection, Collection):
+            self._collection = collection
+            self._database = collection.database
+            self._client = self._database.client
+        elif isinstance(database, Database):
+            self._database = database
+            self._client = self._database.client
+            self._collection = self._database[collection]
+        else:
+            try:
+                self._client = MongoClient(host, port)
+            except:
+                raise ConnectionError('Could not connect to database')
+
+            if database not in self._client.list_database_names():
+                log.info("Database: '%s', does not exist - creating new", database)
+            self._database = self._client[database]
+
+            if collection not in self._database.list_collection_names():
+                log.info("Collection: '%s', does not exist in Database: '%s' - creating new", collection,
+                      self._database.name)
+            self._collection = self._database[collection]
+
+            log.info("Created MongoInterface using Database: '%s', and Collection: '%s'",
+                self._database.name, self._collection.name)
+
+    @property
+    def client(self):
+        return self._client
+
+    @property
+    def database(self):
+        return self._database
+
+    @property
+    def collection(self):
+        return self._collection
+
+    @collection.setter
+    def collection(self, collection: Collection):
+        self._collection = collection
+
+    def get_many_dev_cursor(self, device_type, dev_ids, self_col: Collection = None, start_time: DateTime = None,
+                            end_time: DateTime = None, limit=None, fields=None):
+
+        if self_col is None:
+            self_col = self.collection
+
+        if fields == 'all':
+            select_all_fields = True
+            fields = None
+        else:
+            select_all_fields = False
+
+        if (not isinstance(fields, list)) and (fields is not None):
+            raise ValueError("fields must be list of strings or 'all'")
+
+        if device_type == 'dewh':
+            str_id_list = ["GEY" + str(dev_id) for dev_id in dev_ids]
+            if fields is None:
+                fields = MongoInterface.DEWH_DEFAULT_FIELDS
+        elif device_type == 'pm':
+            str_id_list = ["PM" + str(dev_id) for dev_id in dev_ids]
+            if fields is None:
+                fields = MongoInterface.PM_DEFAULT_FIELDS
+        else:
+            raise ValueError('Device type does not exist or is not supported')
+
+        dev_selection = [{'$and': [{str_id: {'$exists': 1}},
+                                   {'$or': [
+                                       {str_id + "." + field: {'$ne': ""}} for field in fields
+                                   ]}
+                                   ]
+                          } for str_id in str_id_list
+                         ]
+
+        projection = {"_id": 0, 'TimeStamp': 1}
+        if select_all_fields:
+            projection.update({str_id: 1 for str_id in str_id_list})
+        else:
+            projection.update({str_id + "." + field: 1 for field in fields for str_id in str_id_list})
+
+        query = [{'$and': [{"TimeStamp": {'$gte': start_time, '$lte': end_time}},
+                           {'$or': dev_selection}
+                           ]
+                  }, projection
+                 ]
+
+        cur = self_col.find(*query, cursor_type=CursorType.NON_TAILABLE).sort("TimeStamp", 1)
+        if limit is not None:
+            cur = cur.limit(limit)
+
+        return cur
+
+    def get_one_dev_cursor(self, device_type, dev_id, self_col: Collection = None, start_time: DateTime = None,
+                           end_time: DateTime = None, limit=None, fields=None):
+
+        return self.get_many_dev_cursor(device_type, [dev_id], self_col=self_col, start_time=start_time,
+                                        end_time=end_time, limit=limit, fields=fields)
+
+    def get_many_dev_raw_dataframe(self, device_type, dev_ids, self_col: Collection = None, start_time: DateTime = None,
+                                   end_time: DateTime = None, limit=None, fields=None, cursor=None):
+
+        if cursor is None:
+            cursor = self.get_many_dev_cursor(device_type, dev_ids, self_col=self_col, start_time=start_time,
+                                              end_time=end_time, limit=limit, fields=fields)
+
+        list_cur = list(cursor)
+
+        if len(list_cur) == 0:
+            raw_df = pd.DataFrame()
+        else:
+            raw_df = pd.DataFrame(list_cur).set_index('TimeStamp').unstack().dropna()
+            raw_df = pd.DataFrame(raw_df.values.tolist(), index=raw_df.index)
+            raw_df = raw_df[~raw_df.index.duplicated(keep='last')]
+            raw_df = raw_df.unstack(0).reorder_levels([1, 0], axis=1).sort_index(axis=1)
+
+        return raw_df
+
+    def get_one_dev_raw_dataframe(self, device_type, dev_id, self_col: Collection = None, start_time: DateTime = None,
+                                  end_time: DateTime = None, limit=None, fields=None, cursor=None):
+
+        return self.get_many_dev_raw_dataframe(device_type, [dev_id], self_col=self_col, start_time=start_time,
+                                               end_time=end_time, limit=limit, fields=fields, cursor=cursor)
+
+    def set_string_field_to_datetime(self, self_col: Collection = None, field_tag='TimeStamp'):
+        if self_col is None:
+            self_col = self.collection
+
+        # find docs with field tags that are not of datetime format
+        cur = self_col.find({field_tag: {'$not': {'$type': "date"}}}, {field_tag: 1})
+
+        bulk_requests = []
+        for doc in cur:
+            timestamp = parse_datetime(doc[field_tag])
+            bulk_requests.append(pym.UpdateOne({
+                '_id': doc['_id']
+            }, {
+                '$set': {
+                    field_tag: timestamp
+                }
+            }))
+
+        if bulk_requests:
+            result = self_col.bulk_write(bulk_requests).modified_count
+        else:
+            result = 0
+
+        print("Modified TimeStamp type for {} documents".format(result))
+        return result
+
+    # todo not completed
+    def set_string_field_to_float(self, self_col: Collection = None, field_tag='GEY1.Temp'):
+        if self_col is None:
+            self_col = self.collection
+
+        # find docs with field tags that are not of a certain type
+        cur = self_col.find({field_tag: {'$not': {'$type': "double"}}}, {field_tag: 1})
+
+        bulk_requests = []
+        for doc in cur:
+            try:
+                value = float(deep_get(doc, field_tag.split('.')))
+            except ValueError:
+                value = float('nan')
+            except TypeError:
+                value = float('nan')
+
+            bulk_requests.append(pym.UpdateOne({
+                '_id': doc['_id']
+            }, {
+                '$set': {
+                    field_tag: value
+                }
+            }))
+
+        if bulk_requests:
+            result = self_col.bulk_write(bulk_requests).modified_count
+        else:
+            result = 0
+
+        print("Modified field type for {} documents".format(result))
+        return result
+
+    def append_from_collection(self, from_col: Collection, self_col: Collection = None, limit=None, newest=True,
+                               override=False):
+        if self_col is None:
+            self_col = self.collection
+
+        if from_col.count_documents({}) == 0:
+            raise ValueError('Source _collection is empty, nothing to copy')
+        if not override and from_col == self_col:
+            raise ValueError('from_col == self_col, cannot append to self from self, unless override is set')
+
+        order = pym.DESCENDING if newest else pym.ASCENDING
+        if limit is None:
+            cur = from_col.find({}, {'_id': 0}).sort('_id', order)
+        else:
+            cur = from_col.find({}, {'_id': 0}).sort('_id', order).limit(limit)
+
+        requests = []
+        for doc in cur:
+            requests.append(pym.InsertOne(doc))
+
+        try:
+            result = self_col.bulk_write(requests)
+        except PymErrors.BulkWriteError as bwe:
+            raise IndexError("BulkWriteError with errmsg: {}".format(bwe.details['writeErrors'][0]['errmsg'])) from None
+
+        return result.inserted_count
+
+    def clone_from_collection(self, from_col: Collection, self_col: Collection = None, limit=None, newest=True,
+                              override=False):
+
+        if (self_col is not None) and (self_col != self._collection):
+            self.collection = self_col
+            print("MongoInterface now using Collection: '{}'".format(self.collection.name))
+
+        order = pym.DESCENDING if newest else pym.ASCENDING
+        if limit is None:
+            pipeline = [{'$sort': {'_id': order}},
+                        {'$project': {'_id': 0}},
+                        {'$out': self.collection.name}
+                        ]
+        else:
+            pipeline = [{'$sort': {'_id': order}},
+                        {'$limit': limit},
+                        {'$project': {'_id': 0}},
+                        {'$out': self.collection.name}
+                        ]
+
+        if not override and from_col == self.collection:
+            raise ValueError('from_col == self_col, cannot clone self to self, unless override is set')
+        elif not override and self.collection.count_documents({}) > 0:
+            raise ValueError("Output database already contains items, set override to force")
+        else:
+            from_col.database.command('aggregate', from_col.name, pipeline=pipeline, explain=False)
+
+        num_cloned = self.collection.count_documents({})
+        print("Cloned {} documents".format(num_cloned))
+        return num_cloned
+
+    def close(self):
+        self.client.close()
+
+
+if __name__ == '__main__':
+    import timeit
+    from timeit import default_timer as Timer
+
+    mi = MongoInterface(collection='Kikuyu', database='site_data')
+    # mi.clone_collection(kik)
+    mi.set_string_field_to_datetime()
+    mi.collection.create_index("TimeStamp")
+
+    start_date = DateTime(2018, 7, 28, 12, 0)
+    end_date = DateTime(2018, 7, 30, 12, 3)
+
+    st = Timer()
+    raw_df = mi.get_many_dev_raw_dataframe('pm', [0], start_time=start_date, end_time=end_date, fields='all')
+
+    print("Time taken is {}".format(Timer() - st))
+
+    st = Timer()
+    df = raw_df.apply(pd.to_numeric, errors='coerce')
+    print("Time taken is {}".format(Timer() - st))
+    print(raw_df.head(2))
+
+    # raw_df = mi.get_one_dev_cursor('dewh', 5, start_time=start_date, end_time=end_date)
+    # raw_df = list(raw_df)
+    # raw_df = pd.DataFrame(raw_df).set_index('TimeStamp')
+    # raw_df = pd.DataFrame(raw_df.iloc[:,0].values.tolist(),index=raw_df.index, columns=['Temp', 'Status', 'Error'])
+    #
+    # df2 = mi.get_one_dev_cursor('dewh', 6, start_time=start_date, end_time=end_date)
+    # df2 = list(df2)
+    # df2 = pd.DataFrame(df2).set_index('TimeStamp')
+    # df2 = pd.DataFrame(df2.iloc[:, 0].values.tolist(), index=df2.index, columns=['Temp', 'Status', 'Error'])
+    #
+    # raw_df = mi.get_one_dev_cursor('pmo', 0, start_time=start_date, end_time=end_date)
+    # raw_df = list(raw_df)
+    # raw_df = pd.DataFrame(raw_df).set_index('TimeStamp')
+    #
+    #
+    # raw_df = pd.DataFrame(raw_df.iloc[:, 0].values.tolist(), index=raw_df.index, columns=[
+    # 'ForActiveEnergy_Tot']).astype(float)
+    # print(raw_df)
+    # raw_df.plot()
+
+    # print(raw_df)
+
+    # raw_df = raw_df.reset_index().drop_duplicates(subset='TimeStamp').set_index('TimeStamp')
+    #
+    # df1 = raw_df.resample('1min', closed='right').ffill(1)
+    # df2 = raw_df.resample('1min', closed='left').ffill(1)
+    # raw_df = raw_df.resample('1min').ffill(1)
+    #
+    # df1['isNaN'] = np.isnan(df1.Temp.values).astype(np.int)
+    # df1.isNaN = (df1.isNaN.rolling(15).mean() >= 1).astype(np.int)
+    # df1['Temp2'] = df1.Temp + 50
+    # df1['Status2'] = df1.Status * 10 - 20
+    # df1.Temp = df1.Temp.interpolate()
+    # df1.Status = df1.Status.ffill()
+    #
+    # df2['isNaN'] = np.isnan(df1.Temp.values).astype(np.int)
+    # df2.isNaN = (df2.isNaN.rolling(15).mean() >= 1).astype(np.int)
+    # df2.Temp = df2.Temp.interpolate()
+    # df2.Status = df2.Status.ffill()
+    #
+    # # df1 = df1.resample('15Min', closed='right').mean()
+    # df2 = df2.resample('15Min').mean()
+    #
+    # import matplotlib.pyplot as plt
+    #
+    # plt.style.use('default')
+    #
+    # df1.Status *= 10
+    #
+    # df1.isNaN -= 2
+    #
+    # # plt.figure()
+    # # raw_df.Temp.plot()
+    # # plt.figure()
+    # fig, ax = plt.subplots(1, 1)
+    # df1.plot(drawstyle="steps-post", ax=ax)
+    # # df2.plot(drawstyle="steps-post", ax=ax)
+    # # plt.figure()
+    # # df1.isNaN.plot(drawstyle="steps-post")
+    #
+    # print(df1.describe())
+    #
+    # plt.show()
